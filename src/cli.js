c9d9695d96a2063641f069c026f1f74c8e7973d4
Semyon Talalaev <talalaev.sv@gmail.com>
2021-05-08 14:39:09 +0300

    feat: added eslint

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const name = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-
+  console.log('Welcome to the Brain Games!');
+  const askingQuestion = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${askingQuestion}!`);
 };
 
 export { name };
