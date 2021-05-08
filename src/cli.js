b3133658e69c0603de2430112ab8be0d22f56b64
Semyon Talalaev <talalaev.sv@gmail.com>
2021-05-08 15:32:01 +0300

    change named export to export def

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const name = () => {
+export default () => {
   console.log('Welcome to the Brain Games!');
   const askingQuestion = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${askingQuestion}!`);
 };
-
-export { name };
